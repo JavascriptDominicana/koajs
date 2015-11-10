95aca29fa234d9e10346b21443f8982f766cb695
Leonardo Jimenez <leonardo@codetiger.co>
2015-11-10 12:19:47 -0400

    Included mongodb in the application

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const Koa = require('koa');
 const app = new Koa();
 const route = require('koa-route');
 const views = require('koa-views');
+//const mongo = require('koa-mongo');
+const monk = require('monk');
+const wrap = require('co-monk');
+
+var db = monk('localhost/pokedex');
+var pokedex = wrap(db.get('pokemons'));
 
 app.use(views('views', {
 
@@ -12,13 +18,6 @@ app.use(views('views', {
 
 }));
 
-var db = [
-  { name: 'bulbasaur',  type: 'Grass', id: 1, image: 'http://img.pokemondb.net/artwork/bulbasaur.jpg'},
-  { name: 'charmander', type: 'Fire', id: 4, image: 'http://img.pokemondb.net/artwork/charmander.jpg'},
-  { name: 'squirtle', type: 'water', id: 7, image: 'http://img.pokemondb.net/artwork/squirtle.jpg'},
-  { name: 'pidgey', type: 'flying', id: 16, image: 'http://img.pokemondb.net/artwork/pidgey.jpg'},
-]
-
 var pokemons = {
 
   index: function*(){
@@ -26,12 +25,13 @@ var pokemons = {
   },
 
   list: function *(){
-    var pokemons = db;
+    var pokemons = yield pokedex.find({});// db;  //this.mongo.collection('pokemons').find();
+    console.log(pokemons);
     yield this.render('list', {'pokemons': pokemons });
   },
 
   show: function *(name){
-    var pokemon = get_pokemon(name);
+    var pokemon = yield pokedex.findOne({name: name});
 
     if(pokemon){
       yield this.render('show', {'pokemon': pokemon});
@@ -41,18 +41,6 @@ var pokemons = {
   },
 
 };
-
-function get_pokemon(name){
-
-  var pokedex = db;
-
-  for( var i = 0, length = pokedex.length; i < length; i++){
-    if(pokedex[i].name == name){
-      return pokedex[i];
-    }
-  }
-  return false;
-}
 
 app.use(route.get('/', pokemons.index));
 app.use(route.get('/pokemons', pokemons.list));
